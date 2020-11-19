9d9256bb79b5fbcbdfaf10aa60e5fb8dbb265080
lucianoz98 <lucianozg98@gmail.com>
2020-11-19 16:53:04 -0500

    Avance 4.3

--- Graph/parser.h
+++ Graph/parser.h
@@ -8,8 +8,9 @@
 #include "UndirectedGraph.h"
 #include "DirectedGraph.h"
 #include "rapidjson/document.h"
-#include "rapidjson/writer.h"
+#include "rapidjson/ostreamwrapper.h"
 #include "rapidjson/stringbuffer.h"
+#include "rapidjson/istreamwrapper.h"
 
 using namespace std;
 using namespace rapidjson;
@@ -20,10 +21,28 @@ void clear(){
 } //Clears parser saved values
 
 void readJSON(){
-    Document mundo_parse;
-    Document peru_parse;
-    peru_parse.Parse("./pe.json");
-    mundo_parse.Parse("./airports.json");
+    ifstream peru_parse {R"(pe.json)"};
+    if (!peru_parse.is_open()){
+        cerr << "No se puede leer el archivo seleccionado";
+        return;
+    }
+
+    IStreamWrapper isw { peru_parse };
+    Document peru_doc {};
+    peru_doc.ParseStream(isw);
+    StringBuffer buffer {};
+
+    if (peru_doc.HasParseError()){
+        cout << "Error : " << peru_doc.GetParseError() << '\n'
+             << "Offset: " << peru_doc.GetErrorOffset() << '\n';
+        return;
+    }
+
+    for (auto itr = peru_doc.Begin(); itr != peru_doc.End(); ++itr){
+        string Ciudad = (*itr)["City"].GetString(), Nombre = (*itr)["Name"].GetString();
+        float Longitud = stof((*itr)["Longitude"].GetString()), Latitud = stof((*itr)["Latitude"].GetString());
+        int Id = stoi((*itr)["Airport Id"].GetString()), Adyacentes = stoi((*itr)["destinations"].GetString());
+    }
 } //Parser JSON file and saves data into class
 
 void uGraphMake(UnDirectedGraph<string, double> &tempGraph){
