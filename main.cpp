03bba96925499389068c7711f41bd1003ea1a845
jose <jose.vasquez.a@utec.edu.pe>
2020-11-21 11:48:22 -0500

    avance 5

--- main.cpp
+++ main.cpp
@@ -60,12 +60,23 @@ void testDirectedGraph()
         cin>>id1;
         cout<<"ID2 => ";
         cin>>id2;
+        while (graph.findById(id1) && graph.findById(id2)!=1)
+        {
+            cout<<"\nIDs invalidos, intente de nuevo"<<endl;
+            cout<<"\nInserte Arista:"<<endl;
+            cout<<"ID1 => ";
+            cin>>id1;
+            cout<<"ID2 => ";
+            cin>>id2;
+        }
+
         cout<<"Peso de la arista => ";
         cin>>peso;
         graph.createEdge(id1,id2,peso);
         cout<<"Quiere seguir insertando aristas al grafo? (si/no)"<<endl;
         cin>>flag_loop;
     }
+    int opcion_switch;
     std::cout << "================================================" << std::endl;
     cout<<"Escoja una funcion:"<<endl;
     cout<<"\n1. Eliminar un vertice"<<endl;
@@ -77,11 +88,12 @@ void testDirectedGraph()
     cout<<"7. Verificar si el grafo esta fuertemente conectado"<<endl;
     cout<<"8. Verificar si el grafo esta vacio"<<endl;
     cout<<"9. Eliminar el grafo"<<endl;
-    cin>>opcion;
+    cout<<"10. Mostrar el grafo"<<endl;
+    cin>>opcion_switch;
     std::cout << "================================================" << std::endl;
-    switch (opcion)
+    switch (opcion_switch)
     {
-    case '1':
+    case 1:
         flag_loop="si";
         while (flag_loop=="si")
         {
@@ -93,15 +105,72 @@ void testDirectedGraph()
             cin>>flag_loop;
         }
         break;
+
+    case 2:
+        flag_loop="si";
+        while (flag_loop=="si")
+        {
+            string id1,id2;
+            cout<<"\nQue arista quiere eliminar?"<<endl;
+            cout<<"ID1 => ";
+            cin>>id1;
+            cout<<"ID2 => ";
+            cin>>id2;
+            graph.deleteEdge(id1,id2);
+            cout<<"Quiere seguir eliminando aristas? (si/no)"<<endl;
+            cin>>flag_loop;
+        }
+        break;
     
-    case '4':
+    case 3:
+        flag_loop="si";
+        while (flag_loop=="si")
+        {
+            string id1,id2;
+            cout<<"\nElija la arista"<<endl;
+            cout<<"ID1 => ";
+            cin>>id1;
+            cout<<"ID2 => ";
+            cin>>id2;
+            cout<<"El peso de la arista es => "<<graph(id1,id2);
+            cout<<"Quiere conocer otro peso? (si/no)"<<endl;
+            cin>>flag_loop;
+        }
+        break;
+    
+    case 4:
         cout<<"\nLa densidad del grafo es: "<<graph.density()<<endl;
+        break;
+    
+    case 5:
+        cout<<"\nEl grafo tiene la  densidad minima requerida? "<<std::boolalpha<<graph.isDense()<<endl;
+        break;
+    
+    case 6:
+        cout<<"\nEl grafo es conectado? "<<std::boolalpha<<graph.isConnected()<<endl;
+        break;
+    
+    case 7:
+        cout<<"\nEl grafo es fuertemente conectado? "<<std::boolalpha<<graph.isStronglyConnected()<<endl;
+        break;
+    
+    case 8:
+        cout<<"\nEl grafo es vacio? "<<std::boolalpha<<graph.empty()<<endl;
+        break;
+
+    case 9:
+        graph.empty();
+        cout<<"\nGrafo eliminado!"<<endl;
+        break;
+    case 10:
+        cout<<"\nEl grafo creado es"<<endl;
+        graph.display();
         break;
 
     default:
         break;
     }
-    graph.display();
+
 }
 
 void testUnDirectedGraph()
